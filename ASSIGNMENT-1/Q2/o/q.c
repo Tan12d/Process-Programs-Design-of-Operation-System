1e470786d37e56634281917ef53be8564945dc28
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2023-07-18 02:55:41 +0530

    Create q.c

---
+++ ASSIGNMENT-1/Q2/o/q.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+	int i=5;
+
+	if(fork()==0)
+	   i=i+1;
+
+	else
+	   i=i-1;
+
+	fprintf(stderr,"%d",i);
+  
+  return 0;
+}
+
