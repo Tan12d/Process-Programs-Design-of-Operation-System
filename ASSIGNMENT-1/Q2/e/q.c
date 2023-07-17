426da57723231aa777db646dd6ecd72ecd52f2de
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2023-07-18 02:32:41 +0530

    Create q.c

---
+++ ASSIGNMENT-1/Q2/e/q.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+	pid_t pid;
+	int i=5;
+  pid=fork();
+  
+	if(pid==0)
+	{
+		i=i+1;
+		printf("Child: %d",i);
+	}
+
+  else
+	{
+		wait(NULL);
+		printf("Parent: %d",i);
+	}
+
+	return 0;
+}
+
